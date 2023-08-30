22e2906b686be2df0038114dc26d3b0b0139d8b8
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-30 17:33:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-window.promises = [];
+
+        function getRandomTime(min, max) {
+            return Math.random() * (max - min) + min;
+        }
+
+        function delay(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        function createRandomPromise() {
+            return new Promise(resolve => {
+                const randomTime = getRandomTime(1000, 5000); // Random time between 1 and 5 seconds
+                setTimeout(() => {
+                    resolve(`Promise resolved after ${randomTime} milliseconds`);
+                }, randomTime);
+            });
+        }
+
+        const promises = Array.from({ length: 5 }, createRandomPromise);
+
+        Promise.any(promises)
+            .then(result => {
+                const outputDiv = document.getElementById('output');
+                outputDiv.textContent = result;
+            })
+	window.promises = [promises];
 
 // Do not change the code above this
 // add your promises to the array `promises`
